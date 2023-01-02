775acbe51e550edfe5325015d2a5fbbbe1ea8507
omodev <omooladev@gmail.com>
2023-01-02 21:06:39 +0100

    finished navigation links

--- src/store/App/AppContextProvider.js
+++ src/store/App/AppContextProvider.js
@@ -1,16 +1,15 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { AppContext } from "./app-context";
 
 const AppContextProvider = (props) => {
   const [appDisplayMode, setAppDisplayMode] = useState("light");
   const changeDisplayMode = (mode) => {
-    document.body.classList.toggle("dark")
+    document.body.classList.toggle("dark");
     setAppDisplayMode((prevMode) => {
       return mode;
     });
   };
 
- 
   return (
     <AppContext.Provider
       value={{
