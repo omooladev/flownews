ac8f3831e3f2bc9015360b3c9bc5280e84aee31f
omodev <omooladev@gmail.com>
2023-01-03 11:42:05 +0100

    finished the header

--- src/components/Header/Account.js
+++ src/components/Header/Account.js
@@ -1,13 +1,43 @@
+import { useCallback, useState } from "react";
 import { FaRegUser } from "react-icons/fa";
 import styles from "./Account.module.css";
 const Account = (props) => {
+  const [openAccount, setOpenAccount] = useState(false);
   let className = props.className || "";
+
+  const showOpenAccountContainer = useCallback(() => {
+    setOpenAccount((prevState) => {
+      return true;
+    });
+  }, []);
+  const hideOpenAccountContainer = useCallback(() => {
+    setOpenAccount((prevState) => {
+      return false;
+    });
+  }, []);
+
   return (
-    <div className={`${className} ${styles.account_subscribe}`}>
+    <div className={`${className} ${styles.account_subscribe} ${openAccount ? styles.active : ""}`}>
       <div className={styles.my_account}>
-        <FaRegUser className={styles.icon} />
-        <span>my account</span>
+        <div onMouseOver={showOpenAccountContainer}>
+          <FaRegUser className={styles.icon} />
+          <span>my account</span>
+        </div>
+
+        <div
+          className={styles["my_account_container"]}
+          onMouseOut={hideOpenAccountContainer}
+          onMouseOver={showOpenAccountContainer}
+        >
+          <span>Get into your account</span>
+          <hr />
+          <div className={styles["my_account_container_button"]}>
+            <button className={styles.login}>Login</button>
+            <button className={styles.contributor}>Become a contributor</button>
+          </div>
+        </div>
       </div>
+
       <button type="button" className={styles.subscribe}>
         subscribe
       </button>
