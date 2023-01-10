a4b611d3affc1d0b36c93e4d6cff14398dc33244
omodev <omooladev@gmail.com>
2023-01-10 20:44:33 +0100

    removed the import React statement from ProfileBox component.
     This is actually not compulsory for the latest version of react

--- src/components/Contributor/Dashboard/ProfileBox/ProfileBox.js
+++ src/components/Contributor/Dashboard/ProfileBox/ProfileBox.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import styles from './ProfileBox.module.css';
+
+import styles from "./ProfileBox.module.css";
 
 const ProfileBox = () => {
-	return (
-		<details className={styles.details}>
-			<summary>Click me</summary>
-			<nav className={`${styles['nav-user']}`}>
-				<ul className={`${styles['nav-user-list']}`}>
-					<li>Profile</li>
-					<li>Lists</li>
-					<li>Stories</li>
-					<li>Stats</li>
-				</ul>
-				<hr />
-				<ul className={`${styles['nav-user-list']}`}>
-					<li>Settings</li>
-					<li>Refine recommendations</li>
-					<li>Manage publications</li>
-				</ul>
-				<hr />
-				<ul className={`${styles['nav-user-list']}`}>
-					<li>Become a member</li>
-					<li>Apply to the partner program</li>
-					<li>Gift a membership</li>
-				</ul>
-			</nav>
-		</details>
-	);
+  return (
+    <details className={styles.details}>
+      <summary>Click me</summary>
+      <nav className={`${styles["nav-user"]}`}>
+        <ul className={`${styles["nav-user-list"]}`}>
+          <li>Profile</li>
+          <li>Lists</li>
+          <li>Stories</li>
+          <li>Stats</li>
+        </ul>
+        <hr />
+        <ul className={`${styles["nav-user-list"]}`}>
+          <li>Settings</li>
+          <li>Refine recommendations</li>
+          <li>Manage publications</li>
+        </ul>
+        <hr />
+        <ul className={`${styles["nav-user-list"]}`}>
+          <li>Become a member</li>
+          <li>Apply to the partner program</li>
+          <li>Gift a membership</li>
+        </ul>
+      </nav>
+    </details>
+  );
 };
 
 export default ProfileBox;
