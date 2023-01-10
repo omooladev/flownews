0e0cbdd3824f51fd74dfdebc45d6b4f2776627cd
omodev <omooladev@gmail.com>
2023-01-10 23:57:14 +0100

    working

--- src/store/Auth/auth-context.js
+++ src/store/Auth/auth-context.js
@@ -4,8 +4,5 @@ export const AuthContext = React.createContext({
   isLoggedIn: false,
   onLogin: () => {},
   onSignOut: () => {},
-<<<<<<< HEAD
   dummy_contributor_data: {},
-=======
->>>>>>> f1184c671466804f87e8b6c7d1b92992cf3e8eec
 });
