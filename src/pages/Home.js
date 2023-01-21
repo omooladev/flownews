2c2a22a305a7923c82cda94e1f82850586529da0
omodev <omooladev@gmail.com>
2023-01-21 12:31:02 +0100

    removed lastlocation from home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,14 @@
-import { useContext, useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import { AppContext } from "../store/App/app-context";
+// import { useContext, useEffect } from "react";
+// import { useLocation } from "react-router-dom";
+// import { AppContext } from "../store/App/app-context";
 
 const Home = () => {
-  const location = useLocation();
-  const { onSetLastLocation } = useContext(AppContext);
+  // const location = useLocation();
+  // const { onSetLastLocation } = useContext(AppContext);
 
-  useEffect(() => {
-    onSetLastLocation(location.pathname);
-  }, [location.pathname, onSetLastLocation]);
+  // useEffect(() => {
+  //   onSetLastLocation(location.pathname);
+  // }, [location.pathname, onSetLastLocation]);
   return;
 };
 
