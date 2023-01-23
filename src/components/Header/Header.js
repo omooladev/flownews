adc39a70b148a6114a41f1e145d6af334ab19ecc
omosuyi olawole <omooladev@gmail.com>
2023-01-23 10:12:39 -0800

    worked on suspense loader

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,12 +8,14 @@ import Menu from "./Menu";
 import Navigation from "./Navigation";
 
 import ProfileNavigation from "../Contributor/Dashboard/Navigation/ProfileNavigation";
+// import SuspenseLoader from "../Loaders/SuspenseLoader";
 const Header = () => {
   const { toggleMenu } = useContext(AppContext);
   const { isLoggedIn } = useContext(AuthContext);
   return (
     <header className={`${styles.header} ${toggleMenu ? styles.active : ""}`}>
       <Menu />
+      {/* <SuspenseLoader/> */}
       <Logo className={styles.logo} />
       <Navigation className={styles.navigation} />
       {!isLoggedIn && <Account className={styles.account_subscribe} />}
