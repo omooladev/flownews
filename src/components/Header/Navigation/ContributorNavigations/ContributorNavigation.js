32096ec6897c225df6bd759cd4a655a8e5d738fd
omodev <omooladev@gmail.com>
2023-01-24 01:07:00 +0100

    commented a note in the contributor navigation

--- src/components/Header/Navigation/ContributorNavigations/ContributorNavigation.js
+++ src/components/Header/Navigation/ContributorNavigations/ContributorNavigation.js
@@ -1,3 +1,4 @@
+//? NOTE---All navigation links here are for the contributors
 import styles from "./ContributorNavigation.module.css";
 import Write from "./Write";
 import Notification from "./Notification";
