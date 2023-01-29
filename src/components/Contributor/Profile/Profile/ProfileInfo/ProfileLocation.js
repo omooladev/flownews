ae1de7ccab4c06db47a27f4182d6829d4068c91b
omodev <omooladev@gmail.com>
2023-01-29 13:46:34 +0100

    worked on location again

--- src/components/Contributor/Profile/Profile/ProfileInfo/ProfileLocation.js
+++ src/components/Contributor/Profile/Profile/ProfileInfo/ProfileLocation.js
@@ -2,13 +2,16 @@ import { BiLocationPlus } from "react-icons/bi";
 import styles from "./ProfileLocation.module.css";
 const ProfileDate = (props) => {
   let { location } = props;
-  location = location || "";
 
   return (
-    <div className={styles.location}>
-      <BiLocationPlus />
-      <p>{location}</p>
-    </div>
+    <>
+      {location && (
+        <div className={styles.location}>
+          <BiLocationPlus />
+          <p>{location}</p>
+        </div>
+      )}
+    </>
   );
 };
 
