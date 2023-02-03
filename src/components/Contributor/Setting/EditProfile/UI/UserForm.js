52a26f9226e553c649086be2285cbe66595814b8
omosuyi olawole <omooladev@gmail.com>
2023-02-03 00:57:47 -0800

    finished the name field

--- src/components/Contributor/Setting/EditProfile/UI/UserForm.js
+++ src/components/Contributor/Setting/EditProfile/UI/UserForm.js
@@ -1,28 +1,32 @@
-import { useContext } from "react";
+import { useContext, useCallback, useState, useEffect } from "react";
 import { AuthContext } from "../../../../../store/Auth/auth-context";
 import styles from "./UserForm.module.css";
 const UserForm = () => {
   const {
-    userData: { lastname, middlename, firstname },
+    userData: { fullname: name },
   } = useContext(AuthContext);
-  let fullname;
+  const [fullname, setFullname] = useState("");
 
-  if (!lastname && !firstname && !middlename) {
-    fullname = "";
-  }
-  if (lastname) {
-    fullname = lastname;
-  }
-  if (middlename) {
-    fullname += " " + middlename;
-  }
-  if (firstname) {
-    fullname += " " + firstname;
-  }
-  console.log(fullname);
+  const changeFullNameHandler = useCallback((event) => {
+    setFullname((prevValue) => {
+      return event.target.value;
+    });
+  }, []);
 
+  const submitFormHandler = useCallback(
+    (event) => {
+      event.preventDefault();
+      console.log(fullname);
+    },
+    [fullname]
+  );
+  useEffect(() => {
+    if (name) {
+      setFullname((prevValue) => name);
+    }
+  }, [name]);
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={submitFormHandler}>
       <div className={styles.form_controls}>
         <div className={styles.form_control}>
           <label htmlFor="userFormControl__name">Name</label>
@@ -31,7 +35,7 @@ const UserForm = () => {
             id="userFormControl__name"
             placeholder="Please enter your fullname"
             value={fullname}
-            onChange={() => {}}
+            onChange={changeFullNameHandler}
           />
           <p>
             Your name may appear around flownews where you contribute or are mentioned. You can
@@ -39,7 +43,9 @@ const UserForm = () => {
           </p>
         </div>
       </div>
-      <div className={styles.form_actions}></div>
+      <div className={styles.form_actions}>
+        <button type="submit">Save Profile Information</button>
+      </div>
     </form>
   );
 };
