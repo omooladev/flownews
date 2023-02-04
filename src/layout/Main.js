04811e31bf8c2cab38e1731223085ac30d14fe16
omosuyi olawole <omooladev@gmail.com>
2023-02-04 12:20:24 -0800

    worked on the main layout file

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,10 +1,18 @@
-import { useContext } from "react";
+import { useCallback, useContext } from "react";
 import Routes from "../routes/Routes";
 import { AppContext } from "../store/App/app-context";
 const Main = () => {
-  const { onCloseMenu } = useContext(AppContext);
+  const { onCloseMenu, onCloseProfileBox, profileBoxIsActive,toggleMenu } = useContext(AppContext);
+  const closeComponentsHandler = useCallback(() => {
+    if (profileBoxIsActive) {
+      onCloseProfileBox();
+    }
+    if(toggleMenu){
+      onCloseMenu()
+    }
+  }, [onCloseProfileBox,onCloseMenu,profileBoxIsActive,toggleMenu]);
   return (
-    <main className="main" onClick={onCloseMenu}>
+    <main className="main" onClick={closeComponentsHandler}>
       <Routes />
     </main>
   );
