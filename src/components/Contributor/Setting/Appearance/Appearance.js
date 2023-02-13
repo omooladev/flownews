8d5dfc581474d0c026118350011e03e2cd6d1e81
omosuyi olawole <omooladev@gmail.com>
2023-02-13 14:01:50 -0800

    working on the theme mode

--- src/components/Contributor/Setting/Appearance/Appearance.js
+++ src/components/Contributor/Setting/Appearance/Appearance.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { AuthContext } from "../../../../store/Auth/auth-context";
 import EmailVerify from "../UI/EmailVerification/VerifyEmail_CancelEmailRequest";
 import styles from "./Appearance.module.css";
+import ThemeMode from "./ThemeMode";
 const Appearance = () => {
   const {
     userData: { emailIsVerified, emailRequestChange },
@@ -15,6 +16,7 @@ const Appearance = () => {
         Choose how Flownews looks to you. Select a single theme,the default value is the browser
         theme
       </p>
+      <ThemeMode />
     </section>
   );
 };
---
+++ src/components/Contributor/Setting/Appearance/ThemeMode.js
@@ -0,0 +1,11 @@
+
+const ThemeMode = () => {
+  const themes = [{}];
+  return (
+    <section>
+      <label>Theme Mode</label>
+    </section>
+  );
+};
+
+export default ThemeMode;
