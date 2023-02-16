7805122c2c80660191e1ad22cd5aef0ebed8920e
omosuyi olawole <omooladev@gmail.com>
2023-02-16 06:23:19 -0800

    working on the appearcance of the website

--- src/UI/Card.js
+++ src/UI/Card.js
@@ -1,7 +1,12 @@
 import styles from "./Card.module.css";
 const Card = (props) => {
   const className = props.className || "";
-  return <div className={`${styles.card} ${className}`}>{props.children}</div>;
+  const onClick = props.onClick || null;
+  return (
+    <div className={`${styles.card} ${className}`} onClick={onClick}>
+      {props.children}
+    </div>
+  );
 };
 
 export default Card;
--- src/components/Contributor/Setting/Appearance/ThemeMode.js
+++ src/components/Contributor/Setting/Appearance/ThemeMode.js
@@ -1,10 +1,69 @@
+import { useContext } from "react";
+import { AppContext } from "../../../../store/App/app-context";
+import Card from "../../../../UI/Card";
+import styles from "./ThemeMode.module.css";
+import LightDefaultMode from "../../../../assets/light_preview.svg";
+import DarkDefaultMode from "../../../../assets/dark_preview.svg";
+import { useState } from "react";
 
 const ThemeMode = () => {
-  const themes = [{}];
+  const {
+    appMode: { display },
+  } = useContext(AppContext);
+  const [mode, setMode] = useState(display);
+  console.log(mode);
+  const themes = [
+    {
+      name: "Light Default",
+      image: LightDefaultMode,
+      modeName: mode === "light-default-theme" ? styles.lightMode : "",
+      active: mode === "light-default-theme" ? styles.active : "",
+      checked: mode === "light-default-theme",
+    },
+    {
+      name: "Dark Default",
+      image: DarkDefaultMode,
+      modeName: mode === "dark-default-theme" ? styles.darkMode : "",
+      active: mode === "dark-default-theme" ? styles.active : "",
+      checked: mode === "dark-default-theme",
+    },
+  ];
+
+  const changeModeHandler = (name) => {
+    if (name === "Dark Default") {
+      setMode("dark-default-theme");
+    }
+    if (name === "Light Default") {
+      setMode("light-default-theme");
+    }
+  };
   return (
-    <section>
+    <>
       <label>Theme Mode</label>
-    </section>
+      <form className={styles.theme_mode}>
+        {themes.map((theme) => {
+          return (
+            <Card
+              key={theme.name}
+              className={`${styles.single_theme} ${theme.modeName} ${theme.active}`}
+            >
+              <img src={theme.image} alt={theme.name} />
+              <hr />
+              <div className={styles.theme_details}>
+                <input
+                  type="radio"
+                  value={mode}
+                  name="theme-mode"
+                  checked={theme.checked}
+                  onChange={() => changeModeHandler(theme.name)}
+                />
+                <label htmlFor="theme-mode">{theme.name}</label>
+              </div>
+            </Card>
+          );
+        })}
+      </form>
+    </>
   );
 };
 
