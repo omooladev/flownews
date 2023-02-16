ae8c6b00e1adbcc8f5d8e7e1d12d1513ce397847
omosuyi olawole <omooladev@gmail.com>
2023-02-16 14:09:24 -0800

    created inputs and labels of the password authentication components

--- src/components/Contributor/Setting/Access/PasswordAuthentication/PasswordAuthentication.js
+++ src/components/Contributor/Setting/Access/PasswordAuthentication/PasswordAuthentication.js
@@ -1,5 +1,42 @@
+import { useContext } from "react";
+import { AuthContext } from "../../../../../store/Auth/auth-context";
+import EmailVerify from "../../UI/EmailVerification/VerifyEmail_CancelEmailRequest";
+import styles from "../../EditProfile/UI/UserForm/UserForm.module.css";
 const PasswordAuthentication = () => {
-  console.log("Password authentication");
-  return;
+  const {
+    userData: { emailIsVerified, emailRequestChange },
+  } = useContext(AuthContext);
+  return (
+    <section>
+      {(!emailIsVerified || emailRequestChange) && <EmailVerify />}
+      <h2>Change Password</h2>
+      <hr />
+      <form className={styles.form}>
+        <div className={styles.form_controls}>
+          <div className={styles.form_control}>
+            <label>Old Password</label>
+            <input type="password" />
+          </div>
+          <div className={styles.form_control}>
+            <label>New password</label>
+            <input type="password" />
+          </div>
+          <div className={styles.form_control}>
+            <label>Confirm new password</label>
+            <input type="password" />
+          </div>
+        </div>{" "}
+        <div className={styles.form_actions}>
+          <button
+            type="submit"
+            className={styles.update_profile}
+            // disabled={isLoading ? true : false}
+          >
+            Update password
+          </button>
+        </div>
+      </form>
+    </section>
+  );
 };
 export default PasswordAuthentication;
