bdea7b1b9b55daeb277922738b9039297524f948
omoola <omooladev@gmail.com>
2023-03-23 14:06:17 -0700

    selected font family

---
+++ src/notes.js
@@ -0,0 +1,4 @@
+//* fonts family
+//? primary fonts will be used for the logo of the website
+//? secondary fonts will be used for the navigation links and contents of the website
+//? tertiary will  be used for page headers
