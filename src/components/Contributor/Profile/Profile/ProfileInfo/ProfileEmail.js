6b430f8305ebbdb08dbb0aaf0acf04e1f589d926
omodev <omooladev@gmail.com>
2023-04-16 21:47:20 +0100

    working on the responsiveness for profile

--- src/components/Contributor/Profile/Profile/ProfileInfo/ProfileEmail.js
+++ src/components/Contributor/Profile/Profile/ProfileInfo/ProfileEmail.js
@@ -7,7 +7,7 @@ const ProfileEmail = (props) => {
     <div className={styles.email}>
       <BiEnvelope />
       <p>{email}</p>
-      {!emailIsVerified && <BiXCircle className={styles.not_verified} />}
+      {!emailIsVerified && <BiXCircle className={styles.not_verified} title="email not verified" />}
     </div>
   );
 };
