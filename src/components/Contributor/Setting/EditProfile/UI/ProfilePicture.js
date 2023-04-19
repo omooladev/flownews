e6af2af943ebe685a82dceac55b84e6b52e737d0
omodev <omooladev@gmail.com>
2023-04-19 18:24:56 +0100

    refactoring userform

--- src/components/Contributor/Setting/EditProfile/UI/ProfilePicture.js
+++ src/components/Contributor/Setting/EditProfile/UI/ProfilePicture.js
@@ -5,8 +5,9 @@ const ProfilePicture = () => {
   return (
     <section className={styles.profile_picture}>
       <p>Profile picture</p>
-      <ProfileBox className="UserForm__ProfileBox"/>
-      <button>Edit</button>
+      <ProfileBox className="UserForm__ProfileBox" />
+      {/* <button>Edit</button> */}
+      {/* <input type="radio" /> */}
     </section>
   );
 };
