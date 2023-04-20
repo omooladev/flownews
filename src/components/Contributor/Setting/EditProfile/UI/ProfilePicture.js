a98201c74e6393f0f81eaff40dac4d0eaeffb82a
omodev <omooladev@gmail.com>
2023-04-20 11:12:07 +0100

    refactoring the userform profile picture

--- src/components/Contributor/Setting/EditProfile/UI/ProfilePicture.js
+++ src/components/Contributor/Setting/EditProfile/UI/ProfilePicture.js
@@ -1,12 +1,39 @@
+import { FaEdit } from "react-icons/fa";
 import ProfileBox from "../../../../Header/Navigation/ContributorNavigations/ProfileBox";
 import styles from "./ProfilePicture.module.css";
+import { useCallback, useContext, useEffect, useState } from "react";
+import { AppContext } from "../../../../../store/App/app-context";
 
 const ProfilePicture = () => {
+  const {
+    onToggleComponentsIsActive,
+    componentsIsActive: { uploadContainerIsActive },
+  } = useContext(AppContext);
+  const toggleUploadContainer = useCallback(
+    (event) => {
+      event.stopPropagation();
+      onToggleComponentsIsActive({ type: "uploadContainer", event: "toggle" });
+    },
+    [onToggleComponentsIsActive]
+  );
+
   return (
     <section className={styles.profile_picture}>
       <p>Profile picture</p>
       <ProfileBox className="UserForm__ProfileBox" />
-      {/* <button>Edit</button> */}
+      <button
+        onClick={toggleUploadContainer}
+        className={uploadContainerIsActive ? styles.active : ""}
+      >
+        <FaEdit />
+        <span>Edit</span>
+      </button>
+      {uploadContainerIsActive && (
+        <ul className={styles.upload_container}>
+          <li>Upload a photo...</li>
+          <li>Revert Avatar</li>
+        </ul>
+      )}
       {/* <input type="radio" /> */}
     </section>
   );
