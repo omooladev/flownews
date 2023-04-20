619c4c9d0b074dd5aa6b15618301f780bed6bd1a
omodev <omooladev@gmail.com>
2023-04-20 18:00:17 +0100

    workign on the profile picture

--- src/components/Contributor/Setting/EditProfile/UI/ProfilePicture.js
+++ src/components/Contributor/Setting/EditProfile/UI/ProfilePicture.js
@@ -1,10 +1,11 @@
-import { FaEdit } from "react-icons/fa";
+import { FaRegEdit } from "react-icons/fa";
 import ProfileBox from "../../../../Header/Navigation/ContributorNavigations/ProfileBox";
 import styles from "./ProfilePicture.module.css";
-import { useCallback, useContext } from "react";
+import { useCallback, useContext, useState } from "react";
 import { AppContext } from "../../../../../store/App/app-context";
 
 const ProfilePicture = () => {
+  const [profilePicture, setProfilePicture] = useState("");
   const {
     onToggleComponentsIsActive,
     componentsIsActive: { uploadContainerIsActive },
@@ -16,33 +17,42 @@ const ProfilePicture = () => {
     },
     [onToggleComponentsIsActive]
   );
-  const stopPropagationHandler = useCallback(
-    (event) => {
-      event.stopPropagation();
-    },
-    []
-  );
+  const stopPropagationHandler = useCallback((event) => {
+    event.stopPropagation();
+  }, []);
+
+  const saveProfilePictureHandler = useCallback((event) => {
+    event.stopPropagation();
+    onToggleComponentsIsActive({ type: "uploadContainer", event: "close" });
+    const file = event.target.files[0];
+
+    console.log(file.size);
+  }, []);
 
   return (
     <section className={styles.profile_picture}>
       <p>Profile picture</p>
+      {/* <p>Please upload a picture smaller than 1MB</p> */}
       <ProfileBox className="UserForm__ProfileBox" />
       <button
         onClick={toggleUploadContainer}
         className={uploadContainerIsActive ? styles.active : ""}
       >
-        <FaEdit />
+        <FaRegEdit />
         <label htmlFor="userForm_upload_container">Edit</label>
       </button>
       {uploadContainerIsActive && (
         <ul className={styles.upload_container} id="userForm_upload_container">
           <li onClick={stopPropagationHandler}>
             <label htmlFor="userForm_image_input">Upload a photo...</label>
-            <input type="file" accept="image/" id="userForm_image_input" />
           </li>
-          <li>
-            <label>Revert Avatar</label>
-          </li>
+          <input
+            type="file"
+            accept="image/"
+            id="userForm_image_input"
+            onClick={stopPropagationHandler}
+            onChange={saveProfilePictureHandler}
+          />
         </ul>
       )}
     </section>
