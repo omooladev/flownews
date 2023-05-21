4b7f48330de6e31aafa6e59f8d55d5bf0315f31f
omodev <omooladev@gmail.com>
2023-05-21 20:24:37 +0100

    added extra classname to the main class

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -6,7 +6,7 @@ import Routes from "../routes/Routes";
 const Main = () => {
   const { onToggleComponentsIsActive } = useContext(AppContext);
 
-  const { profileUpdated } = useContext(AuthContext);
+  const { profileUpdated, lastLocation } = useContext(AuthContext);
 
   const closeComponentsHandler = useCallback(
     (event) => {
@@ -16,9 +16,16 @@ const Main = () => {
     },
     [onToggleComponentsIsActive]
   );
+
+  //important------->className explanation
+  //note----->For the class name, we check current page the user is on,
+  //note----->and if this page is the profile page then we add a class name of profile_page.
+  //note----->This is because we want to add configure some extra settings
   return (
     <main
-      className={`main ${profileUpdated ? "profile_updated" : ""}`}
+      className={`main ${profileUpdated ? "profile_updated" : ""} ${
+        lastLocation === "profile" ? "profile_page" : ""
+      }`}
       onClick={closeComponentsHandler}
     >
       <Routes />
--- src/pages/Contributor/Profile/Profile.js
+++ src/pages/Contributor/Profile/Profile.js
@@ -4,11 +4,21 @@ import ProfileCmp from "../../../components/Contributor/Profile/Profile";
 import useFetchContributorData from "../../../hooks/useFetchContributorData";
 import { AuthContext } from "../../../store/Auth/auth-context";
 import { useTitle } from "../../../hooks/useTitle";
+import useNewLocation from "../../../hooks/useNewLocation";
 const Profile = () => {
   // useCloseProfileUpdated()
+  //note-------->fetches contributor data
   useFetchContributorData();
+
+  //note-------->saves route location
+  useNewLocation("profile");
+
+  //note-------->Get contributor Data
   const { contributorData } = useContext(AuthContext);
+
+  //note-------->Save title of this page
   useTitle(`${contributorData.username}`);
+
   return <>{contributorData.username && <ProfileCmp />}</>;
 };
 
