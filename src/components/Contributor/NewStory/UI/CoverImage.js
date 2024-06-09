d7faeb9a255210394f538936138bda09571952d1
Omosuyi Olawole <omooladev@gmail.com>
2024-06-09 16:55:01 +0100

    validated the cover image

--- src/components/Contributor/NewStory/UI/CoverImage.js
+++ src/components/Contributor/NewStory/UI/CoverImage.js
@@ -1,8 +1,25 @@
+//<---------- import modules ---------->
+import { useCallback, useState } from "react";
+import { validateFile } from "../../../../utils/validateFile";
+import styles from "./CoverImage.module.css";
+
 const CoverImage = () => {
+  const [message, setMessage] = useState({ type: null, text: "" });
+  const changeImageHandler = useCallback(async (event) => {
+    const file = event.target.files[0];
+    const { error, hasError } = await validateFile({ file, type: "image", from: "cover-image" });
+    if (hasError) {
+      return setMessage((prevMessage) => {
+        return { ...prevMessage, type: "error", text: error };
+      });
+    }
+  }, []);
+
   return (
     <div className={styles["cover-image"]}>
       <label htmlFor="story-cover-image">Add a cover image</label>
-      <input type="file" id="story-cover-image" />
+      <input type="file" accept="image/*" id="story-cover-image" onChange={changeImageHandler} />
+      {message.type === "error" && <p>{message.text}</p>}
     </div>
   );
 };
--- src/config.js
+++ src/config.js
@@ -6,5 +6,6 @@ export const configuration = {
   maxLengthOfWork: 100,
   userFormInputDelay: 300, //0.3s
   maxProfilePictureSize: 1024 * 1024 * 1, //? This is 1MB
+  maxImageSize: 1024 * 1024 * 1, //? This is 1MB
   minLengthOfPassword: 8,
 };
